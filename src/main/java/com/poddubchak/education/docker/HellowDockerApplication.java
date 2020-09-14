e7a06b5cd98ca76c99f21b4a589e234e87189a6f
VladimirPoddubchak <45694858+VladimirPoddubchak@users.noreply.github.com>
2020-09-14 13:11:23 +1000

    Init commit

---
+++ src/main/java/com/poddubchak/education/docker/HellowDockerApplication.java
@@ -0,0 +1,13 @@
+package com.poddubchak.education.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poddubchak/education/docker/HellowDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poddubchak.education.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
